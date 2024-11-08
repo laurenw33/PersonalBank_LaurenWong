22048a5712ec73cda029f6861226252a9652cf20
BT_2S14_14 <laurenw33@nycstudents.net>
2024-11-08 10:52:28 -0500

    Established if-else lines for creating new accounts and developing arrays to store usernames and pins.

--- src/Runner.java
+++ src/Runner.java
@@ -6,7 +6,27 @@ public class Runner {
 
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        System.out.println("Would you like to create a new account?");
+        String[] usernames;
+
+        System.out.print("Would you like to create a new account? ");
+        String answer = s.nextLine();
+        if (answer.equals("yes") == true)
+        {
+            System.out.print("What would you like your username to be? ");
+            String user = s.nextLine();
+            usernames = {user};
+        }
+        if (answer.equals("no") == true)
+        {
+            System.out.print("Please enter your username: ");
+            String access = s.nextLine();
+            if (usernames.length = 0)
+            {
+
+            }
+        }
+
+
 
 
 
